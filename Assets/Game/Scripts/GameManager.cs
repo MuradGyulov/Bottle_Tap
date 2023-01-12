b801bb07b537a32e07c4553dea9489f6f37b53ac
MuradGyulov <80948753+MuradGyulov@users.noreply.github.com>
2023-01-12 18:05:52 +0300

    Так себе был день.
    Так себе была и свадьба, хотя в свадбах я не разбираюсь.
    Почти весь день болтался в машине, а когда не болтался, мерз на улице.
    В общем притомился я, и не смогу написать осмысленный коментарий, но разработка
    идет в правильном напровлении.

---
+++ Assets/Game/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [SerializeField] private int collectStars;
+    [SerializeField] private Animator fadeAnimator;
+
+    private int starsCounter = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void StarsCounter()
+    {
+        starsCounter++;
+
+        if(starsCounter == collectStars)
+        {
+            Invoke("FadePanel", 3.5f);
+            Invoke("LoadNextlevel", 4.5f);
+        }
+    }
+
+    public void FadePanel()
+    {
+        fadeAnimator.SetBool("FadePanelDisapperance", true);
+    }
+
+    public void LoadNextlevel()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
